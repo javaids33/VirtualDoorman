754880bec719f2fc9dd433636e1efc22e2a2edaa
Syed Javaid <s.java458@gmail.com>
2025-05-28 19:50:46 -0400

    Adding test setup.sh file.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# echo "Ensure packages are installed:"
+# sudo apt-get install python3-numpy git firefox-esr
+
+echo "Clone repositories:"
+git clone https://github.com/javaids33/VirtualDoorman
+cd vd
+
+sudo chmod -rwx vd
